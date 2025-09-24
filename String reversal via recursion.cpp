5000913e654514b20ac4a0d6197a2d4178299f2d
vaishnavipangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-09-24 05:17:48 -1200

    Create String reversal via recursion.cpp

---
+++ String reversal via recursion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void reverseString(char str[], int start, int end) {
+    if (start >= end) {   
+        return;
+    }
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseString(str, start + 1, end - 1);
+}
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    reverseString(str, 0, length - 1);
+    cout << "Reversed string: " << str << endl;
+    return 0;
+}
+
+
+/*Output:-
+Enter a string: xyz
+Reversed string: zyx
+
+*/
